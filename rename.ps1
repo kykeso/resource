d8f9bb507b8786d8b0206ce43672f50ca124bb40
Ebo <48538796+kykeso@users.noreply.github.com>
2023-05-12 12:48:58 +0800

    Update rename.ps1
    removed foreground

--- rename.ps1
+++ rename.ps1
@@ -1,5 +1,4 @@
-Write-Host "TBS Hostname renamer writen by: Enkhbold.T"
-ForegroundColor Green `
+Write-Host "TBS Hostname renamer writen by: Enkhbold.T" `
   $name = Read-Host -Prompt "Enter New Computer Name" `
   wmic computersystem where name!=null call unjoindomainorworkgroup `
   sleep 10 `
