bc99c0cbf10886c3c46b38966effca55699aa2b5
Ebo <48538796+kykeso@users.noreply.github.com>
2023-05-12 12:43:38 +0800

    Add files via upload

---
+++ rename.ps1
@@ -0,0 +1,9 @@
+Write-Host "TBS Hostname renamer writen by: Enkhbold.T"
+ForegroundColor Green `
+  $name = Read-Host -Prompt "Enter New Computer Name" `
+  wmic computersystem where name!=null call unjoindomainorworkgroup `
+  sleep 10 `
+  Rename-Computer -NewName $name `
+  sleep 10 `
+  add-computer -DomainName "tavanbogd.local" -Credential tavanbogd0\enkhbold.t -force -Options JoinWithNewName, AccountCreate `
+  Restart-Computer -Confirm:$true
